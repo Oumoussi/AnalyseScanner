35b0021de09e577cae1a640e157d87ebc853ebd4
Oumoussi <abderahimamoussi1@gmail.com>
2023-11-27 14:40:55 +0100

    27/11/2023 14:30

--- js/authParams.js
+++ js/authParams.js
@@ -1,5 +1,5 @@
 export const params = {
-    api: "http://localhost/omeka_s/api/",
+    api: "http://localhost/omk_thyp_23-24_clone/api/",
     key: "FOFXfN57JOqC1w3ujCX5e41TqJjMow2w",
     ident: "KG474vk1XHJUy8cuAFpwVflg2wTQsoK9",
     mail: "abderahimamoussi1@gmail.com",
